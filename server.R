4950ba75d584f71e5624378bfce0b3fda3875d48
Mahesh Gurumoorthi <48827109+gmkumar1988@users.noreply.github.com>
2023-06-15 20:57:00 -0400

    Add files via upload

---
+++ server.R
@@ -0,0 +1,35 @@
+library(shiny)
+library(UsingR)
+data(diamond)
+
+shinyServer(function(input, output) {
+  
+  model <- lm(price ~ carat, data = diamond)
+  
+  
+  modelpred <- reactive({
+    CaratInput <- input$sliderCarat
+    predict(model, newdata = data.frame(carat = CaratInput))
+  })
+  
+  
+  output$plot <- renderPlot({
+    CaratInput <- input$sliderCarat
+    
+    plot(diamond$carat, diamond$price, xlab = "Mass (carats)", 
+         ylab = "Price (SIN $)", bty = "n", pch = 16,
+         xlim = c(0, 6), ylim = c(0, 20000))
+    if(input$showModel){
+      abline(model, col = "red", lwd = 2)
+    }
+    points(CaratInput, modelpred(), col = "red", pch = 16, cex = 2)
+    
+    
+  })
+  
+  output$pred <- renderText({
+    modelpred()
+  })
+  
+  
+})
---
+++ ui.R
@@ -0,0 +1,17 @@
+library(shiny)
+shinyUI(fluidPage(
+  titlePanel("Predicting the price of a diamond"),
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("sliderCarat", "What is price of a diamond?", 0, 6, value = 0.1),
+      checkboxInput("showModel", "Show/Hide regression line", value = TRUE),
+      submitButton("Submit") 
+    ),
+    mainPanel(
+      plotOutput("plot"),
+      h3("Predicted price of a diamond:"),
+      textOutput("pred"),
+      
+    )
+  )
+))
